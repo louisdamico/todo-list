103877f8980dd86dda453926fc43a16c127f8629
loudam88 <loudam88@gmail.com>
2021-02-01 17:59:57 -0600

    User Auth and Todo set up

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -25,10 +25,9 @@ urlpatterns = [
     path('login/', views.loginuser, name='loginuser'),
     path('logout/', views.logoutuser, name='logoutuser'),
 
-
-    
     # To-dos
     path('', views.home, name='home'),
+    path('create/', views.createtodo, name='createtodo'),
     path('current/', views.currenttodos, name='currenttodos'),
 
 ]
