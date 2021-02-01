990454e9abcf746b0cd63c50a45b58b1dd7a5472
loudam88 <loudam88@gmail.com>
2021-02-01 13:36:54 -0600

    Set up user authentication

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -15,7 +15,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from todo import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # Auth
+    path('signup/', views.signupuser, name='signupuser'),
+    path('login/', views.loginuser, name='loginuser'),
+    path('logout/', views.logoutuser, name='logoutuser'),
+
+
+    
+    # To-dos
+    path('', views.home, name='home'),
+    path('current/', views.currenttodos, name='currenttodos'),
+
 ]
